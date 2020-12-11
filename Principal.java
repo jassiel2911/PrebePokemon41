27378095ec553eecc73eee646a13a8d36cf78bc6
jassiel2911 <bjbp.29.11.98@outlook.com>
2020-12-11 00:48:29 -0600

    Creacion de menu

--- Principal.java
+++ Principal.java
@@ -1,10 +1,19 @@
 public class Principal{
     public static void main(String[] args){
         System.out.println("*******BIENVENIDO A POKEMON*******");
-        System.out.println("Quieres elegir a tus pokemones o que te sean asignados aleatoriamente?");
-        //Implementar el menú
-        System.out.println("Mostrar pokemones disponibles para la batalla.");
+        System.out.println("Pokemones disponibles para la batalla.");
+        System.out.println("Apodo: " +/**/ "Vida base: " + /**/ "Defensa base: " + /**/ "Velocidad base: " 
+        + /**/ "Estado inicial: " + /**/ "Tipo: " /* + */);
         Colecciones a = new Colecciones();
         a.listarPokemones();
+        int eleccion = 1;
+        System.out.println("Quieres elegir a tus pokemones (1) o que te sean asignados aleatoriamente (2)?");
+        if(eleccion == 1){
+            System.out.println("Elige a tus pokemones");
+        }else{
+            if(eleccion == 2){
+                System.out.println("Estos son tus pokemones para pelear: ");
+            }
+        }
     }
 }
