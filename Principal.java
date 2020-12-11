5311d83615621f76eb8dc5e3e9793da4a15aebf2
jassiel2911 <bjbp.29.11.98@outlook.com>
2020-12-11 00:07:20 -0600

    Otro commit

--- Principal.java
+++ Principal.java
@@ -1,5 +1,5 @@
 public class Principal{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("*******BIENVENIDO A POKEMON*******");
         System.out.println("Quieres elegir a tus pokemones o que te sean asignados aleatoriamente?");
         //Implementar el menú
