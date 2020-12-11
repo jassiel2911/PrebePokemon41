df2a4e908e12a147079262c7ebd7709c52afc52d
jassiel2911 <bjbp.29.11.98@outlook.com>
2020-12-11 01:09:10 -0600

    Menu

--- Principal.java
+++ Principal.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class Principal{
     public static void main(String[] args){
         System.out.println("*******BIENVENIDO A POKEMON*******");
         System.out.println("Pokemones disponibles para la batalla.");
-        System.out.println("Apodo: " +/**/ "Vida base: " + /**/ "Defensa base: " + /**/ "Velocidad base: " 
-        + /**/ "Estado inicial: " + /**/ "Tipo: " /* + */);
-        Colecciones a = new Colecciones();
-        a.listarPokemones();
-        int eleccion = 1;
+        //System.out.println("Apodo: " +/**/ "Vida base: " + /**/ "Defensa base: " + /**/ "Velocidad base: " 
+        //+ /**/ "Estado inicial: " + /**/ "Tipo: " /* + */);
+        
+        /*Colecciones a = new Colecciones();
+        a.listarPokemones();*/
+
+        int eleccion;
+        Scanner op = new Scanner(System.in);
         System.out.println("Quieres elegir a tus pokemones (1) o que te sean asignados aleatoriamente (2)?");
+        eleccion = op.nextInt(); 
         if(eleccion == 1){
             System.out.println("Elige a tus pokemones");
         }else{
